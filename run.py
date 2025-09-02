f0e9c63a5800cb28fd4b6eef279c98091cfac21e
Luiz Togni <contato.togni@gmail.com>
2025-09-02 15:11:13 -0300

    feat: add variables settings in run server

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 from src.api import app
+from src.core.config import settings
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(
+        host=settings.APP_HOST,
+        port=settings.APP_PORT,
+        debug=settings.APP_DEBUG,
+        load_dotenv=True
+    )
